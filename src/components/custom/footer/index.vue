beb514032dfadbcc315853a077213558936b8030
杨家祺 <abc13380692294@gmail.com>
2025-10-31 20:50:12 +0800

    zhdp-shopping-src

---
+++ src/components/custom/footer/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="footer">欢迎浏览智慧农业大棚商城</div>
+</template>
+
+<style scoped>
+.footer {
+  text-align: center; /* 水平居中 */
+  font-size: 20px; /* 字体大小 */
+  line-height: 50px; /* 垂直居中（可选） */
+}
+</style>
